964753207bdf2b590de81e1c8efdb50a738cb591
Sylar-3 <mohamadkhir1999@gmail.com>
2021-07-04 12:50:41 +0300

    it's coming

--- src/ball.js
+++ src/ball.js
@@ -1,80 +1,113 @@
-import * as THREE from 'three'
-import * as CANNON from 'cannon-es'
-import { body2mesh } from './body2mesh'
+import * as THREE from "three";
+import * as CANNON from "cannon-es";
+import { body2mesh } from "./body2mesh";
 
 export class Ball {
+  constructor(radius, position) {
+    this.ballBody = new CANNON.Body({
+      shape: new CANNON.Sphere(radius),
+      mass: 100,
+      linearDamping: 0.2,
+    });
+    this.ballMesh = body2mesh(this.ballBody);
+    console.log(this.ballMesh, this.ballBody);
+    this.position = position;
+  }
 
-	constructor(radius, position) {
-		this.ballBody= new CANNON.Body({
-			shape: new CANNON.Sphere(radius),
-			mass: 100,
-			linearDamping: 0.2
-		})
-		this.ballMesh = body2mesh(this.ballBody)
-		console.log(this.ballMesh, this.ballBody)
-		this.position = position
-	}
+  set position(pos) {
+    const { x, y, z } = pos;
+    if (!!this.ballMesh) {
+      this.ballMesh.position.x = x;
+      this.ballMesh.position.y = y;
+      this.ballMesh.position.z = z;
+    }
+    if (!!this.ballBody) {
+      this.ballBody.position.x = x;
+      this.ballBody.position.y = y;
+      this.ballBody.position.z = z;
+    }
+  }
 
-	set position(pos) {
-		const { x, y, z } = pos
-		if (!!this.ballMesh) {
-			this.ballMesh.position.x = x
-			this.ballMesh.position.y = y
-			this.ballMesh.position.z = z
-		}
-		if (!!this.ballBody) {
-			this.ballBody.position.x = x
-			this.ballBody.position.y = y
-			this.ballBody.position.z = z
-		}
-	}
+  get position() {
+    return this.ballBody.position;
 
-	get position() {
-		return this.ballBody.position
+    /* position getter */
+  }
 
-		/* position getter */
-	}
+  set mass(mass) {
+    this.ballBody.mass = mass;
 
-	set mass(mass) {
-		this.ballBody.mass = mass
+    /* mass setter */
+  }
 
-		/* mass setter */
-	}
+  get mass() {
+    return this.ballBody.mass;
+  }
 
-	get mass() {
-		return this.ballBody.mass
-	}
+  set friction(fric) {
+    this.ballBody.linearDamping = fric;
+  }
 
-	set friction(fric) {
-		this.ballBody.linearDamping = fric
-	}
+  get friction() {
+    return this.ballBody.linearDamping;
+  }
 
-	get friction() {
-		return this.ballBody.linearDamping
-	}
+  set velocity(fric) {
+    this.ballBody.velocity = fric;
+  }
 
-	set velocity(fric) {
-		this.ballBody.velocity = fric
-	}
+  get velocity() {
+    return this.ballBody.linearDamping;
+  }
 
-	get velocity() {
-		return this.ballBody.linearDamping
-	}
+  weightFunction(mass, gravity) {
+    /* weight formula W = m.g */
+    this.weightForce = mass * gravity;
+  }
 
-	applyForce(vector) {
-		this.ballBody.applyForce(vector)
+  applyForce(vector) {
+    /* F = m*a */
+    /* vy = a*t + v0 */
+    this.hitForce = this.mass * 80;
+    this.weightFunction(this.mass, -9.8);
+    this.ballBody.applyForce(vector);
+    this.reactionForce;
+  }
 
-		/* F = m*a */
-		/* vy = a*t + v0 */
-		this.hitForce = (this.mass)*80
-	}
+  applyImpulse(vector) {
+    /* calculating speed using motion formula */
+    /* x = vx.t + x0 */
+    Dx = this.position.x2 - this.position.x1;
+    Dy = this.position.y2 - this.position.y1;
+    Dz = this.position.z2 - this.position.z1;
 
-	applyImpulse(vector) {
-		this.ballBody.applyImpulse(vector)
+    this.speedOnX = Dx;
+    this.speedOnY = Dy;
+    this.speedOnZ = Dz;
+    this.ballBody.applyImpulse(vector);
+  }
 
-		/* calculating speed using motion formula */
-		/* x = vx*t + x0 */
-		this.speed = this.velocity
-	}
+  reactionForce() {
+    /* R = W */
+    /* Vector of (R) = -Vector of (W) */
+    this.Reaction = -1 * this.weightForce;
+  }
 
+  keneticEnergy() {
+    /* Ek = 0.5 m.v^2 */
+    Ek = 0.5 * this.mass * Math.pow(this.velocity, 2);
+    this.kenetic = Ek;
+  }
+
+  potentEnergy() {
+    /* Ep = m.g.h */
+    Ep = 9.8 * this.mass * this.position.y;
+    this.potent = Ep;
+  }
+
+  energyCalculation() {
+    /* E = Ek + Ep */
+    E = this.kenetic + this.potent;
+    this.energy = E;
+  }
 }
